0bf0736143d3a074b7680fb399806fd8b25f75aa
Makkkiiii <devendradennis@gmail.com>
2025-01-27 12:08:07 +0545

    Changes made to css

--- JS/script.js
+++ JS/script.js
@@ -41,5 +41,19 @@ document.addEventListener("DOMContentLoaded", function () {
 // ? SIDEBAR HIDE SHOW
 
 const sidebar = document.querySelector("aside");
-const showSidebarBtn = document.querySelector("#show-sidebar-btn");
-const hideSidebarBtnebar = document.querySelector("#hide-sidebar-btn");
+const showSidebarBtn = document.querySelector("#show_sidebar-btn");
+const hideSidebarBtn = document.querySelector("#hide_sidebar-btn");
+
+const showSidebar = () => {
+  sidebar.style.left = 0;
+  showSidebarBtn.style.display = "none";
+  hideSidebarBtn.style.display = "inline-block";
+};
+showSidebarBtn.addEventListener("click", showSidebar);
+
+const hideSidebar = () => {
+  sidebar.style.left = "-100%";
+  showSidebarBtn.style.display = "inline-block";
+  hideSidebarBtn.style.display = "none";
+};
+hideSidebarBtn.addEventListener("click", hideSidebar);
